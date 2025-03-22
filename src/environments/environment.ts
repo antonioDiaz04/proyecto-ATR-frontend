a5c2cc8d1c19e29878ff12f0858af76ac5c3e7c3
eduardocastro465 <152236610+eduardocastro465@users.noreply.github.com>
2025-03-22 10:16:15 -0600

    redireccionamiento al backend-render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,19 @@
-export const environment = {
-  production: false,
-  // api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
-  api: 'http://localhost:4000/api/v1', //!Local
-  serviceWorker: true,
-  publicKey:"BLYrs4NfSkkjBeVIMgH2ANO4Hcd3FrGKYXabe5L6jpURiePXV9cbl4IewwLjzdszahWAb8SaiPNmyMz6mywr6KY",
-  privateKey:"keH3GwgD0noSJ7IHg_DLwMRP5WM8T0xlRRb2OG-bjFI",
+Exportador estúpido Ambiente = {
+  Productora: Falso,
+  API: 'https://proyecto-atr-backend.onrender.com/api/v1', //! alojado
+  // API: 'http: // localhost: 4000/api/v1', //! Local
+  trabajador Delaware servicio: verdadero,
+  tecla Pública:"Blyrs4nfskkjBevimgh2ano4hcd3frgkyxabe5l6jpuriepxv9cbl4iewwljdszahwab8saipnmymz6mywry",
+  llave privada:"keh3gwgd0nosj7ihg_dlwmrp5wm8t0xlrrb2og-bjfi",
   
-  //para la autenticacion con firebase
-  firebaseConfig : {
-    apiKey: "AIzaSyB1qHkNRaakPbvjccyQjs49fHf5MH1HGPU",
-    authDomain: "atelier-bd93b.firebaseapp.com",
-    projectId: "atelier-bd93b",
-    storageBucket: "atelier-bd93b.firebasestorage.app",
-    messagingSenderId: "452411722311",
-    appId: "1:452411722311:web:eafb14f82195652909bf62",
-    measurementId: "G-Z85LGNMKKZ"
+  //Para La Autenticacion Con Firebase
+  Firebaseconfig : {
+    apagado: "Aizasyb1qhknraakpbvjccyqjs49fhf5mh1hgpu",
+    autotomento: "atelier-bd93b.firebasepp.com",
+    proyectid: "atelier-bd93b",
+    StorageBucket: "atelier-bd93b.firebaseStorage.App",
+    Messagingingsenderid: "452411722311",
+    aplicar: "1:452411722311:Web: EAFB14F82195652909BF62",
+    medición: "G-z85lgnmkkz"
   }
 };
