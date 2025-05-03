403ed6653f23025e6320f1bc9af91cda6b28aa47
antonioDiaz04 <20221136@uthh.edu.mx>
2025-05-03 11:38:49 -0600

    notificaciion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@ export const environment = {
   api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
   // api: 'http://localhost:4000/api/v1', //!Local
   serviceWorker: true,
-  publicKey:"BLYrs4NfSkkjBeVIMgH2ANO4Hcd3FrGKYXabe5L6jpURiePXV9cbl4IewwLjzdszahWAb8SaiPNmyMz6mywr6KY",
-  privateKey:"keH3GwgD0noSJ7IHg_DLwMRP5WM8T0xlRRb2OG-bjFI",
-  //para la autenticacion con firebase
+  
+  publicKey:"BFa7J38DsRrmv8rAvYPj-FJtDptkDdKNwwllIFVfY5kyQ53ZOUnD1ZijkYD4mPNKjB2ZcXBdnhy93pQEV5mxU5w",
+  privateKey:"h2Ktdk_-MgCRhP0JlNaAMWRVEwwFwvjbczHMisFxg84",
+    //para la autenticacion con firebase
   firebaseConfig : {
     apiKey: "AIzaSyB1qHkNRaakPbvjccyQjs49fHf5MH1HGPU",
     authDomain: "atelier-bd93b.firebaseapp.com",
