0116635a24ecf60d36cfa5eb12d70688af9befa9
N.Antonios <134122438+antonioDiaz04@users.noreply.github.com>
2025-06-18 04:18:29 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
+  api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
   api: 'http://localhost:4000/api/v1', //!Local
   serviceWorker: true,
   
